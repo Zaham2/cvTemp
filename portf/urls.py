53582243bea506e3f8c299904c5d5bb46f3b7c86
zaham2 <zaham2@yahoo.com>
2019-09-05 15:19:22 +0200

    Static mediaFiles path removed from urlpatterns[]. This line caused an error

--- portf/urls.py
+++ portf/urls.py
@@ -19,8 +19,14 @@ from django.urls import path
 # --------------
 from django.conf import settings
 from django.conf.urls.static import static
+
 # ---------------
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]  + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+]
+
+# This line caused trouble
+# Deleted for now
+# This will affect media files in this project
+# ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
